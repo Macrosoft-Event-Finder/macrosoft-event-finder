eb186371e6c960cfa73c46dac203666a3c0636fe
Stephanie Hayden <shayden8@student.gsu.edu>
2024-03-27 12:44:38 -0400

    All of the Flask Files

---
+++ readOnlyFlask/websiteFile.py
@@ -0,0 +1,17 @@
+import os
+from website import create_app, db
+from website.models import User, PastInput
+from flask_migrate import Migrate
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, PastInput=PastInput)
+@app.cli.command()
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
