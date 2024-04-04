fc5f42d0fc515587982fdbca98d02d5a62aff145
Stephanie Hayden <shayden8@student.gsu.edu>
2024-04-04 11:28:49 -0400

    Created the final database in models.py

--- placeholder/website/models.py
+++ placeholder/website/models.py
@@ -5,14 +5,24 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from website import login_manager
 
+
+#the join table. Establishes many to many relationship between users and events
+event_user=db.Table('event_user',
+    db.Column('user_id',db.Integer,db.ForeignKey('user.id')),
+    db.Column('event_id',db.Integer,db.ForeignKey('event.id')))  
+
+#the User table with login functionalities
 class User(UserMixin, db.Model):
     __tablename__='users'
     id=db.Column(db.Integer, primary_key=True)
     
-    username=db.Column(db.String(32), unique=True)
+    email=db.Column(db.String,unique=True)
     password=db.Column(db.String)
     password_hash = db.Column(db.String(128))
-
+    first_name=db.Column(db.String)
+    last_name=db.Column(db.String)
+    
+    account_balance=db.Column(db.Numeric(precision=15,scale=2),default=0)
     @property
     def password(self):
         raise AttributeError('password is not a readable attribute')
@@ -23,50 +33,25 @@ class User(UserMixin, db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
-    pastInputs=db.relationship('PastInput',backref='user', order_by='PastInput.dateCreated')
     def __repr__(self):
-        return '<User %r>' %self.username
+        return '<User %r>' %self.email
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-
-class PastInput(db.Model):
-    __tablename__='pastInputs'
-    id=db.Column(db.Integer, primary_key=True)
-    pastInput=db.Column(db.Text)
-    dateCreated=db.Column(db.DateTime)
-    user_id=db.Column(db.Integer, db.ForeignKey('users.id'))
-    def __repr__(self):
-        return '<Input %r>' % self.pastInput
-    
-class User(db.Model):
-        __tablename__='users'
-        userId=db.Column(db.Integer,primary_key=True)
-        firstName=db.Column(db.String)
-        lastName=db.Column(db.String)
-        email=db.Column(db.String)
-        password=db.Column(db.String)
-        accountBalance=db.Column(db.Numeric(precision=15,scale=2),default=0)
-
-class UnionTable(db.Model):
-        __tablename__='union_table'
-        eventId=db.Column(db.Integer)
-        userId=db.Column(db.Integer)
-        isOrganizer=db.Column(db.Boolean)
-
+#event table 
 class Event(db.Model):
      __tablename__='events'
      #establish many events to one category
      event_category_id=db.Column(db.Integer, db.ForeignKey('event_category.id'))
-
-     eventID=db.Column(db.Integer)
+     users=db.relationship('User',secondary=event_user,backref='events')
+     id=db.Column(db.Integer, primary_key=True)
      description=db.Column(db.Text)
-     flier_image_path=db.Column(db.String(255)) #it is not efficient to store images directly in database, we must store the path to the image instead
+     flier_image_path=db.Column(db.String(255)) #it is not efficient to store images directly in database, we must store the path to the image instead. We can also create a default value for the image
      capacity=db.Column(db.Integer)
      location=db.Column(db.String)
-     isPaidOnly=db.Column(db.Boolean)
+     isPaidOnly=db.Column(db.Boolean, default=False)
      startDate=db.Column(db.DateTime)
      endDate=db.Column(db.DateTime)
 
@@ -76,6 +61,15 @@ class EventCategories(db.Model):
      event_category=db.Column(db.String)
     #establish many events to one category
      events=db.relationship('Event',backref='category')
+
+
+class EventOrganizer(db.Model):
+     __tablename__='event_organizers'
+     id=db.Column(db.Integer,primary_key=True)
+     user_id=db.Column(db.Integer, db.ForeignKey('user.id'))
+     event_id=db.Column(db.Integer,db.ForeignKey('event.id'))
+     is_organizer=db.Column(db.Boolean,default=False)
+     
 '''
 One category has many events but one event can only have one category. 
 If you want events to have multiple categories, you can possibly have 
