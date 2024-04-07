fd64e7dc8be503fff32ae4d0fea26de57511370f
pandabang826 <3thanban9@gmail.com>
2024-04-06 21:11:34 -0400

    Updated homepage, create_event, db, forms, views
    Included category selection in homepage sidebar.
    Established more fields for more event data (i.e. payment, location, capacity).
    Updated db, forms, and views accordingly to new event data establishment.

--- placeholder/website/main/forms.py
+++ placeholder/website/main/forms.py
@@ -1,26 +1,34 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, PasswordField, BooleanField, SelectField, DateField,FileField, TimeField
+from wtforms import StringField, SubmitField, TextAreaField, PasswordField, BooleanField, SelectField, DateField,FileField, TimeField, IntegerField
 from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
 
 
 
 class EventForm(FlaskForm):
-    event_title=StringField('Enter event title',validators=[DataRequired()], render_kw={"placeholder": "Enter event title", "class": "input-field"})
+    title=StringField('Enter event title',validators=[DataRequired()], render_kw={"placeholder": "Enter event title", "class": "input-field"})
     description=TextAreaField('Enter event description', validators=[DataRequired()], render_kw={"placeholder": "Enter event description", "class": "input-field"})
+    capacity=IntegerField('Enter capacity', validators=[DataRequired()], render_kw={"placeholder": "Enter capacity", "class": "input-field"})
+
+    location=StringField('Enter location', validators=[DataRequired()], render_kw={"placeholder": "Enter capacity", "class": "input-field"})
     
-    start_date=DateField('Event start date (dd/mm/yyyy)', format='%d/%m/%Y') #render_kw={"placeholder": "DD/MM/YYYY"})
-    start_time = TimeField('Event start time (hour:minute am/pm)', format='%I:%M %p', validators=[DataRequired()])
-    end_date = DateField('Event end date (dd/mm/yyyy)', format='%d/%m/%Y', validators=[DataRequired()])
-    end_time = TimeField('Event end time (hour:minute am/pm)', format='%I:%M %p', validators=[DataRequired()])
+    start_date=DateField('Event start date (dd/mm/yyyy)', validators=[DataRequired()]) #render_kw={"placeholder": "DD/MM/YYYY"})
+    end_date = DateField('Event end date (dd/mm/yyyy)', validators=[DataRequired()])
+
+    start_time = TimeField('Event start time (hour:minute am/pm)', validators=[DataRequired()])
+    end_time = TimeField('Event end time (hour:minute am/pm)', validators=[DataRequired()])
 
     flier_image_path=FileField('Upload flier for event')
-    event_category=SelectField('Select the category that best fits your event',choices=[('social', 'Social'),
+    paymentRequired = BooleanField('Is the event payment required?', validators=[DataRequired()])
+    paymentAmount = IntegerField('If payment required, enter payment amount')
+    event_category=SelectField('Select the category that best fits your event',validators=[DataRequired()], 
+                               choices=[('social', 'Social'),
                                           ('educational', 'Educational'),
                                           ('fine_arts_media', 'Fine Arts and Media'),
                                           ('fundraiser', 'Fundraiser'),
                                           ('meeting', 'Meeting'),
-                                          ('other', 'Other'),
                                           ('service_volunteer', 'Service/Volunteer'),
                                           ('speaker_lecturer', 'Speaker/Lecturer'),
-                                          ('recreational', 'Recreational')])
+                                          ('recreational', 'Recreational'),
+                                          ('other', 'Other'),])
+    
     post_event=SubmitField('Post Event')
--- placeholder/website/main/views.py
+++ placeholder/website/main/views.py
@@ -4,17 +4,43 @@ from langdetect import detect
 from . import main
 from .forms import EventForm
 from .. import db
-from ..models import User
+from ..models import User, Event, EventCategories
 from flask_login import login_required
 
 @main.route('/', methods=['GET','POST'])
 #@login_required
 def homepage():
-    return render_template('homepage.html')
+    events = Event.query.order_by(Event.start_date).all()
+    return render_template('homepage.html', events=events)
 	  
 @main.route('/create_event',methods=['GET','POST'])
 @login_required
 def create_event():
     eventForm = EventForm()
+    if eventForm.validate_on_submit():
+        event = Event(
+            title = eventForm.title.data,
+            description = eventForm.description.data,
+            capacity = eventForm.capacity.data,
+            location = eventForm.location.data,
+            start_date = eventForm.start_date.data,
+            end_date = eventForm.end_date.data,
+            start_time = eventForm.start_time.data,
+            end_time = eventForm.end_time.data,
+            flier_image_path= eventForm.flier_image_path.data,
+            paymentRequired = eventForm.paymentRequired.data,
+            paymentAmount = eventForm.paymentAmount.data,
+        )
+        db.session.add(event)
+        db.session.commit()
+
+        category = EventCategories(
+            event_category = eventForm.event_category.data,
+        )
+        db.session.add(category)
+        db.session.commit()
+
+        flash('Event posted.')
+        return redirect(url_for('main.homepage'))
     return render_template('create_event.html', eventForm=eventForm)
 
